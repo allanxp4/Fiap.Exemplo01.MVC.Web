e497fa75da47571cff19bf84cde313061b0c8fe9
Allan Pires de Araujo <RM73730@fiap.com.br>
2016-11-01 15:32:08 -0200

    Inserção do Select

---
+++ Fiap.Exemplo01.MVC.Web/Enums/EstadoCivil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exemplo01.MVC.Web.Enums
+{
+    enum EstadoCivil
+    {
+        Solteiro,
+        Casado,
+        Outro
+    }
+}
